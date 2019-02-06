54a1b3df2e95f02746a2a5e9bca367f2b6ba1527
Kyle Sargent <sargentakyle@gmail.com>
2019-02-06 11:01:32 -0600

    added saving functionality for testing purposes

--- code/diskAnalyzer.py
+++ code/diskAnalyzer.py
@@ -1,9 +1,9 @@
 import numpy as np
 import cv2 as cv
+import matplotlib.image as mpimg
 from matplotlib import pyplot as plt
 
-def analyzeDisks():
-    edgeDetection('../photos/leafDiskMildew.png')
+def analyzeDisks(x):
     return NotImplemented
 
 
@@ -16,7 +16,7 @@ def edgeDetection(x):
     #use line(s) 26 to remedy the problem
     #comment out line 20 and uncomment line 21 to see this effect
     
-    #Reading of the same photo, in color and grayscale for edgeDetection.
+    #Reading of the same photo, in color for displaying and grayscale for edgeDetection.
     imgC = cv.imread(x,1)
     imgG = cv.imread(x,0)
 
@@ -30,6 +30,9 @@ def edgeDetection(x):
     #for more details.
     edges = cv.Canny(imgG,100,200)
 
+    #Save image into photos folder for now. so can be used in analyzeDisks method()
+    mpimg.imsave("../photos/edges.png", edges)
+    
 
     #creation of subplots
     plt.subplot(121),plt.imshow(imgC)
@@ -39,9 +42,8 @@ def edgeDetection(x):
     plt.show()
 
 def main():
-    #call analyzing
-    analyzeDisks()
+    imgPath = '../photos/leafDiskMildew.png'
+    edgeDetection(imgPath)
    
-
 
 main()
