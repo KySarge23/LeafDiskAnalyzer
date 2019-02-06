c96977aee29bc92120d9aa797115587d4ab86896
Kyle Sargent <sargentakyle@gmail.com>
2019-02-06 08:52:43 -0600

    beginning implementation on analyzeDisks function

--- code/diskAnalyzer.py
+++ code/diskAnalyzer.py
@@ -3,7 +3,7 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 def analyzeDisks():
-    #waiting to implement
+    edgeDetection('../photos/leafDiskMildew.png')
     return NotImplemented
 
 
@@ -13,33 +13,35 @@ def edgeDetection(x):
     #when reading in with OCV, need to change to RGB if showing with matplot
     #if we want pictures to show up in RGB correctly colored.
     #notice when matplot pops up, color is distorted.
-    #use line(s) 20/21 to remedy the problem
-    #comment out line 19 and uncomment line 18 to see this effect.
-    #img = cv.imread(x,1)
-    img = cv.imread(x,0)
-
+    #use line(s) 26 to remedy the problem
+    #comment out line 20 and uncomment line 21 to see this effect
+    
+    #Reading of the same photo, in color and grayscale for edgeDetection.
+    imgC = cv.imread(x,1)
+    imgG = cv.imread(x,0)
 
     #cv.imshow('Original', img)
 
     #Converting img to RGB:
-    #img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    imgC = cv.cvtColor(imgC, cv.COLOR_BGR2RGB)
 
     #use canny algorithm for edge detection
     #see https://docs.opencv.org/3.4/da/d22/tutorial_py_canny.html
     #for more details.
-    edges = cv.Canny(img,100,200)
+    edges = cv.Canny(imgG,100,200)
 
 
     #creation of subplots
-    plt.subplot(121),plt.imshow(img,cmap='gray')
+    plt.subplot(121),plt.imshow(imgC)
     plt.title('Original Image')
     plt.subplot(122),plt.imshow(edges,cmap='gray')
     plt.title('Edge Dectection')
     plt.show()
 
 def main():
-    #retrieve image from photos folder and pass as param.
-    edgeDetection('../photos/leafDiskMildew.png')
+    #call analyzing
+    analyzeDisks()
+   
 
 
 main()
