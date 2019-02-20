5b611e3546419054f8614be7a0c0e1f1b35c8389
erica055 <erica055@live.missouristate.edu>
2019-02-19 18:01:23 -0600

    completed US005-T1: Create a canvas

---
+++ code/AnalyzerGUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+#initializing the root window
+root = Tk()
+#the size of the window
+root.geometry('600x600')
+
+#a blank canvas for our GUI
+c = Canvas(root, height=600, width=600)
+
+root.mainloop()
