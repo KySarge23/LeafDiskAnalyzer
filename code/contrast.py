568ce84188cc33357d300e784743f1f293c8048b
ericagitlin1 <35502807+ericagitlin1@users.noreply.github.com>
2019-03-04 01:11:09 -0600

    Update contrast.py

--- code/contrast.py
+++ code/contrast.py
@@ -7,7 +7,7 @@ def contour_detection(x):
     img = cv.imread(x)
 
     gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
-    ret,gray = cv.threshold(gray,127,255, 0)
+    ret,gray = cv.threshold(gray,127,255, cv.ADAPTIVE_THRESH_MEAN_C)
     mask = np.zeros(gray.shape,np.uint8)
 
     contours, hierarchy = cv.findContours(gray, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
@@ -18,8 +18,6 @@ def contour_detection(x):
         rect_area = w*h
         extent = float(area)/rect_area
         print(extent)
-        cv.drawContours(img,[cnt],0,(0,255,0),2)
-        cv.drawContours(mask,[cnt],0,255,0)
         cv.imshow("IMG", gray)
         cv.waitKey(0)
         cv.destroyAllWindows()
