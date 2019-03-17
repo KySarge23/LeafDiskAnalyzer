82756b848f4fe2355a1d008352549477822e80dc
Kyle Sargent <sargentakyle@gmail.com>
2019-03-17 13:09:56 -0500

    commented out status label

--- code/GUI.py
+++ code/GUI.py
@@ -81,22 +81,22 @@ class analyzerGUI:
         self.dateEntry.grid(row = 3, column = 1, pady=(0,60))
         self.dateEntry.bind("<FocusIn>", clearDateEntry)
 
-        self.status = Label(master, text="Sending inputs...")
+        # self.status = Label(master, text="Sending inputs...")
 
-        self.progress = ttk.Progressbar(master, orient ="horizontal", length = 150, mode= "indeterminate")
-        self.bytes = 0
-        self.maxbytes = 0
+        # self.progress = ttk.Progressbar(master, orient ="horizontal", length = 150, mode= "indeterminate")
+        # self.bytes = 0
+        # self.maxbytes = 0
 
-        def start(self):
-            self.progress["value"] = 0
-            self.maxbytes = 50000
-            self.progress["maximum"] = 50000
-            self.read_bytes()
+        # def start(self):
+        #     self.progress["value"] = 0
+        #     self.maxbytes = 50000
+        #     self.progress["maximum"] = 50000
+        #     self.read_bytes()
 
-        def read_bytes(self):
-            '''simulate reading 500 bytes; update progress bar'''
-            self.bytes += 500
-            self.progress["value"] = self.bytes
-            if self.bytes < self.maxbytes:
-                # read more bytes after 100 ms
-                self.after(100, self.read_bytes)
+        # def read_bytes(self):
+        #     '''simulate reading 500 bytes; update progress bar'''
+        #     self.bytes += 500
+        #     self.progress["value"] = self.bytes
+        #     if self.bytes < self.maxbytes:
+        #         # read more bytes after 100 ms
+        #         self.after(100, self.read_bytes)
